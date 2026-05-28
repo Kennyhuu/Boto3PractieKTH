89d8d0036bfbfc115bb091762e939f3a7a51e16e
Kreyno93 <michael.wagener@neuefische.de>
2026-05-28 16:29:37 +0200

    feat: add public subnet creation (10.0.0.0/26)

---
+++ pub_subnet.py
@@ -0,0 +1,11 @@
+import boto3
+
+
+def create_subnet(vpc_id):
+    ec2 = boto3.client("ec2")
+
+    # Create a subnet in the specified VPC
+    response = ec2.create_subnet(
+        VpcId=vpc_id, CidrBlock="10.0.0.0/26"  # Replace with your desired CIDR block
+    )
+    return response
