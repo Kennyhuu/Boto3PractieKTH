e38d1617f7ab22e73b1445b0107ee996d3ba833b
Khiem Truong Huu <khiem.truonghuu@synpulse.com>
2026-06-04 15:31:58 +0200

    add some comment for adding item with type

--- dynamoDB.py
+++ dynamoDB.py
@@ -1,8 +1,22 @@
 import boto3
 
+'''
+Common DynamoDB types
+When using boto3.client('dynamodb'), you must use:
+
+'S' → String
+'N' → Number (must be a string like '2008')
+'BOOL' → Boolean
+'M' → Map (dictionary/object)
+'L' → List
+'NULL' → Null
+
+'''
 
 def dynamodb_add_item():
-    dynamodb = boto3.client('dynamodb')
+    dynamodb = boto3.client('dynamodb') # boto3.client (low-level / raw API)
+
+    #dynamodb = boto3.resource('dynamodb') # boto3.resource (high-level / Pythonic)
 
     dynamodb.put_item(
         TableName='Music',
